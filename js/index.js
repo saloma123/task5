290753bf739afa851cfe13660f56319d6bccecb1
saloma123 <130926697+saloma123@users.noreply.github.com>
2023-08-07 21:16:53 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// For Task 1 
+
+// 1 - Inctroduce Your Self (Name , age  , faculty , ) in variable and write it in HTML Using JS Note using template Literal
+
+        //Write Code Here
+name = "Salma Tare Abdullah";
+age = 20;
+faculty = "Computer and artificial intelligence";
+department = "Software Engineering";
+salma = `My name is ${name}, I am ${age} years old and I study in ${faculty} faculty ${department} department.`;
+// document.getElementById("root").innerHTML = salma; 
+document.write(salma);
+ 
+        // Note : you can write in HTML using  document.write(varribale_Name) ; 
+
+
+// 2 - You Have Number 
+
+let str = "20000.432423" ;
+m1 = parseFloat(str); //convert String To Number Using to Two different Method 
+console.log(m1); 
+console.log(typeof m1);
+console.log(+str);
+console.log(typeof +str);
+
